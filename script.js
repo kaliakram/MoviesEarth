878970adbd7d1c41e7d173dced1df7389367ad10
kaliakram <kouah.ali.akram@gmail.com>
2025-06-26 19:31:53 +0100

    initial popover design

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_v
   .then(res => {
 	const bigClass=document.getElementById("movies");
 	bigClass.innerHTML="";
-	for(i=0;i<18;i++){
+	for(i=0;i<16;i++){
 		let element=res.results[i];
 		let newElement=document.createElement("div");
 		let newTitle=document.createElement("p");
@@ -20,20 +20,46 @@ fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_v
 		newOverlay.className="movieOverlay";
 		let newImagePath=`https://image.tmdb.org/t/p/original/${element.poster_path}`;
 		newElement.className="movie";
+		newElement.imagePath=`https://image.tmdb.org/t/p/original/${element.poster_path}`;
+		newElement.originalLang=element.original_language;
+		newElement.overview=element.overview;
+		newElement.releaseDate=element.release_date;
 		newTitle.innerHTML=element.original_title;
 		newElement.style.backgroundImage=`url("${newImagePath}")`;
 		newElement.style.backgroundSize="cover";
 		newElement.appendChild(newTitle);
 		newElement.appendChild(newOverlay);
 		bigClass.appendChild(newElement);
+
 };
-console.log(JSON.stringify(res));
   })
   .catch(err => console.error(err));
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
     getPost();
-	
+	let movies=document.getElementById("movies");
+	let popover_container=document.getElementById("popover-container");
+	let popover=document.getElementById("popover");
+	let popover_content=document.getElementById("popover-content");
+	let exit=document.getElementById("exit");
+	movies.addEventListener("click",(event)=>{
+		let movie=event.target.closest(".movie");
+		console.log(movie);
+		if(!movie || !movies.contains(movie)) return;
+			popover_container.style.display="block";
+			document.getElementById("title").innerHTML=movie.children[0].innerHTML;
+			document.querySelector("#overview").innerHTML="";
+			let overview=document.getElementById("overview");
+			document.getElementById("poster").src=movie.imagePath;
+			document.getElementById("loadingCircle").style.display="none";
+			overview.innerHTML=movie.overview;
+			})
+
+	exit.addEventListener("click",()=>{
+			let loading=document.getElementById("loadingCircle");
+			popover_content.style.display="block";
+			popover_container.style.display="none";
+	})
 
 })
